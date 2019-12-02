798b3ca0936cec308ae4c0f0f2dc913e879b3217
KIM DONG HWAN <quaint3@gmail.com>
2019-12-02 14:11:32 +0900

    tdd1-2, 객체 생성 테스트

--- Book/TDD/tdd-code/src/test/java/com/study/tdd/money/DollarTest.java
+++ Book/TDD/tdd-code/src/test/java/com/study/tdd/money/DollarTest.java
@@ -3,29 +3,34 @@ package com.study.tdd.money;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DollarTest {
 
 	class Dollar {
 
-		int amount = 10;
+		int amount;
 
 		public Dollar(int amount) {
-
+			this.amount = amount;
 		}
 
-		void times(int multiplier) {
-
+		Dollar times(int multiplier) {
+			return new Dollar(amount * multiplier);
 		}
 	}
 
 	@Test
 	public void TestMultipiaction() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 
 }
