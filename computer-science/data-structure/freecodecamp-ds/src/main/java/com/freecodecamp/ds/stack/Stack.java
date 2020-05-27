da54ab23e937bf9959e15f31e47215bf04d64199
Han <quaint3@gmail.com>
2020-05-27 11:41:35 +0900

    CS : Stack
    freecodecamp stack code

---
+++ computer-science/data-structure/freecodecamp-ds/src/main/java/com/freecodecamp/ds/stack/Stack.java
@@ -0,0 +1,53 @@
+package com.freecodecamp.ds.stack;
+
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Stack<T> implements Iterable<T> {
+
+  private LinkedList<T> list = new LinkedList<>();
+
+  //stack 생성
+  public Stack() {
+  }
+
+  //하나의 인자를 받아서, stack을 생성
+  public Stack(T firstElem) {
+    push(firstElem);
+  }
+
+  //stack의 사이즈를 반환
+  public int size() {
+    return list.size();
+  }
+
+  //stack push() 기능
+  public void push(T firstElem) {
+    list.push(firstElem);
+  }
+
+  //stack pop() 기능
+  public T pop() {
+    if (isEmpty()) {
+      throw new EmptyStackException();
+    }
+    return list.removeLast();
+  }
+
+  public T peek() {
+    if (isEmpty()) {
+      throw new EmptyStackException();
+    }
+    return list.peekLast();
+  }
+
+  private boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return list.iterator();
+  }
+}
