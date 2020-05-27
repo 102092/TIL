58337266c3290aadd51e5118b104f87b38613665
Han <quaint3@gmail.com>
2020-05-27 10:53:24 +0900

    CS : freecodecamp ds project init

---
+++ computer-science/data-structure/freecodecamp-ds/src/main/java/com/freecodecamp/ds/DsApplication.java
@@ -0,0 +1,13 @@
+package com.freecodecamp.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DsApplication.class, args);
+  }
+
+}
---
+++ computer-science/data-structure/freecodecamp-ds/src/test/java/com/freecodecamp/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freecodecamp.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
