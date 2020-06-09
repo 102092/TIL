89a0118242abc329143c815cd0e3eb34535aada6
Han <quaint3@gmail.com>
2020-06-09 18:46:38 +0900

    TIL : JPA tutorial 시작

---
+++ tech-book/자바-ORM-표준-JPA-프로그래밍/practice/src/main/java/com/hibernate/jpa/Member.java
@@ -0,0 +1,102 @@
+package com.hibernate.jpa;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "MEMBER", uniqueConstraints = {
+    @UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"NAME", "AGE"})})
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
+  private String id;
+
+  @Column(name = "NAME", nullable = false, length = 10)
+  private String userName;
+
+  private Integer age;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+
+  @Lob
+  private String description;
+
+  public Member() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public RoleType getRoleType() {
+    return roleType;
+  }
+
+  public void setRoleType(RoleType roleType) {
+    this.roleType = roleType;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Date getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(Date lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
---
+++ tech-book/자바-ORM-표준-JPA-프로그래밍/practice/src/main/java/com/hibernate/jpa/RoleType.java
@@ -0,0 +1,5 @@
+package com.hibernate.jpa;
+
+public enum RoleType {
+  ADMIN, USER
+}
